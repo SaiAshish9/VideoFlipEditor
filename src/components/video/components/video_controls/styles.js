1444e66fab8ec2ce204468a1c24b170f6a4bb489
saiashish9 <saiashish3760@gmail.com>
2024-08-04 20:38:32 +0530

    k

--- src/components/video/components/video_controls/styles.js
+++ src/components/video/components/video_controls/styles.js
@@ -1,7 +1,10 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  padding-top: 0.5rem;
+  padding-top: 1rem;
 `;
 
-export const PlayIcon = styled.img``;
+export const PlayIcon = styled.img`
+  width: 20px;
+  height: 20px;
+`;
