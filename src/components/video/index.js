f9acd595c24b690a92c176d27d8e69512c27ea42
saiashish9 <saiashish3760@gmail.com>
2024-08-05 03:27:02 +0530

    k

--- src/components/video/index.js
+++ src/components/video/index.js
@@ -1,11 +1,80 @@
-import React from "react";
-import { VideoContainer, VideoContent } from "./styles";
+import React, { useRef, useState, useEffect } from "react";
+import { Overlay, VideoContainer, VideoContent } from "./styles";
 import { DropdownOptions, VideoControls } from "./components";
+import ReactPlayer from "react-player";
+
+const parentHeight = 307;
+const parentWidth = 450;
 
 const VideoPlayer = () => {
+  const ref = useRef(null);
+
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [dragging, setDragging] = useState(false);
+  const [offset, setOffset] = useState({ x: 0, y: 0 });
+
+  const handleMouseDown = (e) => {
+    setDragging(true);
+    setOffset({
+      x: e.clientX - position.x,
+      y: e.clientY - position.y,
+    });
+    document.body.style.cursor = "grabbing";
+  };
+
+  const handleMouseMove = (e) => {
+    if (dragging) {
+      let newX = e.clientX - offset.x;
+      let newY = e.clientY - offset.y;
+
+      // Ensure the overlay doesn't go beyond the horizontal boundaries
+      if (newX < 0) newX = 0;
+      if (newX + 307 > parentWidth) newX = parentWidth - 307;
+
+      // Ensure the overlay doesn't go beyond the vertical boundaries
+      if (newY < 0) newY = 0;
+      if (newY + 307 > parentHeight) newY = parentHeight - 307;
+
+      setPosition({ x: newX, y: newY });
+    }
+  };
+
+  const handleMouseUp = () => {
+    setDragging(false);
+    document.body.style.cursor = "default";
+  };
+
+  useEffect(() => {
+    const initialX = (parentWidth - 305) / 2;
+    const initialY = (parentHeight - 305) / 2;
+    setPosition({ x: initialX, y: initialY });
+  }, [parentWidth, parentHeight]);
+
+  // move to draggable
+
   return (
     <VideoContainer>
-      <VideoContent />
+      <VideoContent>
+        <ReactPlayer
+          ref={ref}
+          muted={true}
+          loop={true}
+          onEnded={() => {}}
+          onProgress={() => {}}
+          height={307}
+          width={"auto"}
+          url={
+            "https://cdn.loom.com/sessions/thumbnails/313bf71d20ca47b2a35b6634cefdb761-00001.mp4"
+          }
+          playing={true}
+        />
+        <Overlay
+          onMouseDown={handleMouseDown}
+          onMouseMove={handleMouseMove}
+          onMouseUp={handleMouseUp}
+          left={`${position.x}px`}
+        />
+      </VideoContent>
       <VideoControls />
       <DropdownOptions />
     </VideoContainer>
--- src/components/video/styles.js
+++ src/components/video/styles.js
@@ -11,9 +11,29 @@ export const VideoContent = styled.div`
   width: 450px;
   border-radius: 6px;
   cursor: pointer;
+  overflow: hidden;
+  position: relative;
+  z-index: 1;
 `;
 
 export const PlayerContainer = styled.div`
   display: flex;
   align-items: center;
+`;
+
+export const Overlay = styled.div`
+  background-color: rgba(0, 0, 0, 0.29);
+  height: 305px;
+  max-height: 305px;
+  min-height: 305px;
+  width: 305px;
+  max-width: 305px;
+  min-width: 305px;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: ${({ left }) => left ?? 0};
+  border: 1px solid #fff;
+  cursor: grab;
+  z-index: 1000;
 `;
