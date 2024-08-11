270094395ddf53109d47e2e2519f13cd8d4db1dc
saiashish9 <saiashish3760@gmail.com>
2024-08-11 21:02:26 +0530

    [Video Flip Editor] - Completed

--- src/App.js
+++ src/App.js
@@ -33,85 +33,94 @@ const App = () => {
   const [currentFrame, setCurrentFrame] = useState(0);
   const [currentRecordedData, setCurrentRecordedData] = useState(null);
 
+  function renderTab(item) {
+    switch (item) {
+      case TABS[0].title:
+        return (
+          <>
+            <Content>
+              <VideoPlayer
+                setPlayerWidth={setPlayerWidth}
+                playerWidth={playerWidth}
+                ref={combinedRefs}
+                setPreviewImage={setPreviewImage}
+                previewImage={previewImage}
+                setVideoBlobUrl={setVideoBlobUrl}
+                videoBlobUrl={videoBlobUrl}
+                isPlaying={isPlaying}
+                setIsPlaying={setIsPlaying}
+                isStartCropperClicked={isStartCropperClicked}
+                isStreamStarted={isStreamStarted}
+                recordedData={recordedData}
+                setRecordedData={setRecordedData}
+                setIsStreamStarted={setIsStreamStarted}
+                currentRecordedData={currentRecordedData}
+                setCurrentRecordedData={setCurrentRecordedData}
+              />
+              <Preview
+                playerWidth={playerWidth}
+                ref={combinedRefs}
+                isStartCropperClicked={isStartCropperClicked}
+                previewImage={previewImage}
+                isPlaying={isPlaying}
+                videoBlobUrl={videoBlobUrl}
+                isStreamStarted={isStreamStarted}
+                currentRecordedData={currentRecordedData}
+                setCurrentRecordedData={setCurrentRecordedData}
+              />
+            </Content>
+            <Footer
+              isStartCropperClicked={isStartCropperClicked}
+              setIsStartCropperClicked={setIsStartCropperClicked}
+              isRemoveCropperClicked={isRemoveCropperClicked}
+              isGeneratePreviewClicked={isGeneratePreviewClicked}
+              setIsRemoveCropperClicked={setIsRemoveCropperClicked}
+              setIsGeneratePreviewClicked={setIsGeneratePreviewClicked}
+              recordedData={recordedData}
+              setRecordedData={setRecordedData}
+              currentRecordedData={currentRecordedData}
+              setCurrentRecordedData={setCurrentRecordedData}
+            />
+          </>
+        );
+      case TABS[1].title:
+        return (
+          <SessionContainer>
+            {recordedData?.length > 0 && (
+              <Content>
+                <VideoContent>
+                  <SessionContent
+                    selectedHeaderItem={selectedHeaderItem}
+                    currentFrame={currentFrame}
+                    setCurrentFrame={setCurrentFrame}
+                    recordedData={recordedData}
+                  />
+                </VideoContent>
+                <SessionParentContainer>
+                  <PreviewText>
+                    Frame {currentFrame} Out Of {recordedData.length} Recorded
+                    Frames:{" "}
+                  </PreviewText>
+                  <pre>
+                    <code>
+                      {JSON.stringify(recordedData[currentFrame - 1], null, 2)}
+                    </code>
+                  </pre>
+                </SessionParentContainer>
+              </Content>
+            )}
+          </SessionContainer>
+        );
+    }
+  }
+
   return (
     <Container>
       <Header
         selected={selectedHeaderItem}
         setSelected={setSelectedHeaderItem}
       />
-      {selectedHeaderItem === TABS[0].title ? (
-        <>
-          <Content>
-            <VideoPlayer
-              setPlayerWidth={setPlayerWidth}
-              playerWidth={playerWidth}
-              ref={combinedRefs}
-              setPreviewImage={setPreviewImage}
-              previewImage={previewImage}
-              setVideoBlobUrl={setVideoBlobUrl}
-              videoBlobUrl={videoBlobUrl}
-              isPlaying={isPlaying}
-              setIsPlaying={setIsPlaying}
-              isStartCropperClicked={isStartCropperClicked}
-              isStreamStarted={isStreamStarted}
-              recordedData={recordedData}
-              setRecordedData={setRecordedData}
-              setIsStreamStarted={setIsStreamStarted}
-              currentRecordedData={currentRecordedData}
-              setCurrentRecordedData={setCurrentRecordedData}
-            />
-            <Preview
-              playerWidth={playerWidth}
-              ref={combinedRefs}
-              isStartCropperClicked={isStartCropperClicked}
-              previewImage={previewImage}
-              isPlaying={isPlaying}
-              videoBlobUrl={videoBlobUrl}
-              isStreamStarted={isStreamStarted}
-              currentRecordedData={currentRecordedData}
-              setCurrentRecordedData={setCurrentRecordedData}
-            />
-          </Content>
-          <Footer
-            isStartCropperClicked={isStartCropperClicked}
-            setIsStartCropperClicked={setIsStartCropperClicked}
-            isRemoveCropperClicked={isRemoveCropperClicked}
-            isGeneratePreviewClicked={isGeneratePreviewClicked}
-            setIsRemoveCropperClicked={setIsRemoveCropperClicked}
-            setIsGeneratePreviewClicked={setIsGeneratePreviewClicked}
-            recordedData={recordedData}
-            setRecordedData={setRecordedData}
-            currentRecordedData={currentRecordedData}
-            setCurrentRecordedData={setCurrentRecordedData}
-          />
-        </>
-      ) : (
-        <SessionContainer>
-          {recordedData?.length > 0 && (
-            <Content>
-              <VideoContent>
-                <SessionContent
-                  selectedHeaderItem={selectedHeaderItem}
-                  currentFrame={currentFrame}
-                  setCurrentFrame={setCurrentFrame}
-                  recordedData={recordedData}
-                />
-              </VideoContent>
-              <SessionParentContainer>
-                <PreviewText>
-                  Frame {currentFrame} Out Of {recordedData.length} Recorded
-                  Frames:{" "}
-                </PreviewText>
-                <pre>
-                  <code>
-                    {JSON.stringify(recordedData[currentFrame - 1], null, 2)}
-                  </code>
-                </pre>
-              </SessionParentContainer>
-            </Content>
-          )}
-        </SessionContainer>
-      )}
+      {renderTab(selectedHeaderItem)}
     </Container>
   );
 };
